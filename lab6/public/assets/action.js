3650d681d9f66eab044cfbd5825c86bc351e8a7a
Irtiza Hasan <ihasa074@uottawa.ca>
2022-03-07 01:15:11 -0500

    cleanup form validation

--- lab6/public/assets/action.js
+++ lab6/public/assets/action.js
@@ -1,19 +1,24 @@
 // jQuery that will "listen" to the html niceSurvey.html
 $(document).ready(function () {
-  $("form").on("submit", function () {
-    $.ajax({
-      type: "POST",
-      url: "/niceSurvey",
-      data: $(this).serializeArray(),
-      success: function () {
-        $("#submitBtn").prop("disabled", "true");
+  const form = $("#form");
+  form.on("submit", function () {
+    if (form[0].checkValidity()) {
+      $.ajax({
+        type: "POST",
+        url: "/niceSurvey",
+        data: $(this).serializeArray(),
+        success: function () {
+          $("#submitBtn").prop("disabled", "true");
 
-        $("#confirmation").removeAttr("hidden");
-        $("#confirmation").text(
-          "Thank you for participating in this user interface suryey!"
-        );
-      },
-    });
+          $("#confirmation").removeAttr("hidden");
+          $("#confirmation").text(
+            "Thank you for participating in this user interface suryey!"
+          );
+        },
+      });
+    }
+
+    form[0].classList.add("was-validated");
     return false;
   });
 });
